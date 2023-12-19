aeb122724aa9ee71b586f79f7809bd3013f4a997
Aamir Naseer <aamirnaseer498@gmail.com>
2023-12-19 09:48:34 +0500

    Committed JSON Response

--- src/test/java/ParsingResponseBody.java
+++ src/test/java/ParsingResponseBody.java
@@ -9,17 +9,17 @@ import static io.restassured.RestAssured.given;
 
 public class ParsingResponseBody {
 
-//    @Test
-//    void testJSONStaticResponse(){
-//
-//        Response response= given()
-//                .when().get("http://localhost:3000/students");
-//
-//        Assert.assertEquals(response.getStatusCode(),200);
-//        Assert.assertEquals(response.header("Content-Type"),"application/json; charset=utf-8");
-//        Assert.assertEquals(response.jsonPath().get("[2].courses[0]").toString(),"javascript");
-//
-//    }
+    @Test
+    void testJSONStaticResponse(){
+
+        Response response= given()
+                .when().get("http://localhost:3000/students");
+
+        Assert.assertEquals(response.getStatusCode(),200);
+        Assert.assertEquals(response.header("Content-Type"),"application/json; charset=utf-8");
+        Assert.assertEquals(response.jsonPath().get("[2].courses[0]").toString(),"javascript");
+
+    }
 
     @Test
     void testJSONDynamicResponse(){
