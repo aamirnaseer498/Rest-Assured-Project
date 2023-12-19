f3dd541e5378b58f1a8c9c9c09f733076206b79f
Aamir Naseer <aamirnaseer498@gmail.com>
2023-12-19 09:47:40 +0500

    Committed JSON Response

--- src/test/java/ParsingResponseBody.java
+++ src/test/java/ParsingResponseBody.java
@@ -1,4 +1,7 @@
+import io.restassured.http.ContentType;
 import io.restassured.response.Response;
+import org.json.JSONArray;
+import org.json.JSONObject;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -6,23 +9,38 @@ import static io.restassured.RestAssured.given;
 
 public class ParsingResponseBody {
 
-    @Test
-    void testJSONStaticResponse(){
-
-        Response response= given()
-                .when().get("http://localhost:3000/students");
-
-        Assert.assertEquals(response.getStatusCode(),200);
-        Assert.assertEquals(response.header("Content-Type"),"application/json; charset=utf-8");
-        Assert.assertEquals(response.jsonPath().get("[2].courses[0]").toString(),"javascript");
-
-    }
+//    @Test
+//    void testJSONStaticResponse(){
+//
+//        Response response= given()
+//                .when().get("http://localhost:3000/students");
+//
+//        Assert.assertEquals(response.getStatusCode(),200);
+//        Assert.assertEquals(response.header("Content-Type"),"application/json; charset=utf-8");
+//        Assert.assertEquals(response.jsonPath().get("[2].courses[0]").toString(),"javascript");
+//
+//    }
 
     @Test
     void testJSONDynamicResponse(){
 
-        Response response= given()
+        Response response= given().contentType(ContentType.JSON)
                 .when().get("http://localhost:3000/students");
+
+        JSONArray jsonArray= new JSONArray(response.asString());
+        int length= jsonArray.length();
+
+        boolean status= false;
+
+        for (int i=0; i<length; i++) {
+            String name= jsonArray.getJSONObject(i).get("name").toString();
+            if (name.equals("Usama")){
+                status= true;
+                break;
+            }
+        }
+
+        Assert.assertTrue(status);
 
     }
 
