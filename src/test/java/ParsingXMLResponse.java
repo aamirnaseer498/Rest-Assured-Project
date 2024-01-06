1c6b3c38d15ee9e240738823d042512f6b7773a4
Aamir Naseer <aamirnaseer498@gmail.com>
2024-01-06 12:33:18 +0500

    Committed XML Response

--- src/test/java/ParsingXMLResponse.java
+++ src/test/java/ParsingXMLResponse.java
@@ -10,18 +10,18 @@ import static org.hamcrest.Matchers.equalTo;
 
 public class ParsingXMLResponse {
 
-//    @Test
-//    void testXMLStaticResponse(){
-//
-//        given()
-//                .when()
-//                .get("http://restapi.adequateshop.com/api/Traveler?page=1")
-//                .then()
-//                .statusCode(200)
-//                .body("TravelerinformationResponse.page",equalTo("1"))
-//                .body("TravelerinformationResponse.travelers.Travelerinformation[0].id",equalTo("11133"));
-//
-//    }
+    @Test
+    void testXMLStaticResponse(){
+
+        given()
+                .when()
+                .get("http://restapi.adequateshop.com/api/Traveler?page=1")
+                .then()
+                .statusCode(200)
+                .body("TravelerinformationResponse.page",equalTo("1"))
+                .body("TravelerinformationResponse.travelers.Travelerinformation[0].id",equalTo("11133"));
+
+    }
 
     @Test
     void testXMLDynamicResponse(){
