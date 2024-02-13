8bb461cbb69dd5bd6043b38b5764e6aef5d14386
Aamir Naseer <aamirnaseer498@gmail.com>
2024-02-13 19:00:08 +0500

    Committed Basic Authentication

---
+++ src/test/java/Authentication.java
@@ -0,0 +1,24 @@
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+public class Authentication {
+
+    @Test
+    void basicAuthentication(){
+
+        given()
+                .auth().basic("postman","password")
+
+                .when()
+                .get("https://postman-echo.com/basic-auth")
+
+                .then()
+                .statusCode(200)
+                .body("authenticated",equalTo(true))
+                .log().all();
+
+    }
+
+}
